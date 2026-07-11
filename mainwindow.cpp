58eb791608ed95ad57333aa8243b46901433d55e
KovalkovN <nikitos3113@icloud.com>
2026-07-11 11:46:44 +0300

    UI: Add modern QSS styling and receipt history

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,25 @@ MainWindow::MainWindow(QWidget *parent)
 
     calcModule = new Calculator(this);
     connect(calcModule, &Calculator::calculationDone, this, &MainWindow::displayResult);
+
+    this->setStyleSheet(
+        "QMainWindow "
+        "QPushButton {"
+        "   background-color: #4CAF50;"
+        "   color: white;"
+        "   font-size: 14px;"
+        "   padding: 10px;"
+        "}"
+        "QPushButton:hover { background-color: #45a049; }"
+        "QTextEdit {"
+        "   background-color: #ffffff;"
+        "   border: 1px solid #cccccc;"
+        "   border-radius: 5px;"
+        "   font-family: 'Courier New';"
+        "   font-size: 13px;"
+        " color: black; "
+        "}"
+        );
 }
 
 MainWindow::~MainWindow()
@@ -29,5 +48,14 @@ void MainWindow::on_calcButton_clicked()
 
 void MainWindow::displayResult(double total)
 {
-    ui->resultLabel->setText(QString("Итого: %1").arg(total));
+    ui->resultLabel->setText(QString("Итого: %1 руб.").arg(total));
+
+    QString type = ui->productTypeCombo->currentText();
+    double p = ui->priceInput->value();
+    int q = ui->qtyInput->value();
+
+    QString receipt = QString(" Товар: %1\n" " Цена: %2 x %3 шт.\n" " Налоги/Скидки учтены.\n"" К оплате: %4 руб.\n")
+                          .arg(type).arg(p).arg(q).arg(total);
+
+    ui->historyEdit->append(receipt);
 }
